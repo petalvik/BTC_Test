ac2a0990ef0c8f91d90067d1f959083ac6a4768a
SmartPetalvik <petalvik@outlook.com>
2017-07-11 14:40:05 +0300

    Classes moved to separate files

--- BTC.Tests/Controllers/HomeControllerTest.cs
+++ BTC.Tests/Controllers/HomeControllerTest.cs
@@ -44,37 +44,4 @@ namespace BTC.Tests.Controllers
             Assert.IsNotNull(result);
         }
     }
-
-    [TestClass]
-    public class HeroRepositoryTest
-    {
-        [TestMethod]
-        public void CreateDeleteTest()
-        {
-            var heroes = new List<Hero> { new Hero(), new Hero() };
-
-            Mock<IRepository<Hero>> mock = new Mock<IRepository<Hero>>();
-
-            mock.Setup(m => m.Get(null, null, "")).Returns(heroes);
-
-            mock.Setup(m => m.Insert(It.IsAny<Hero>())).Callback<Hero>(h => heroes.Add(h));
-
-            mock.Setup(m => m.Delete(It.IsAny<Hero>())).Callback<Hero>(h => heroes.Remove(h));
-
-
-            var repo = mock.Object;
-            Assert.IsNotNull(repo);
-            Assert.IsTrue(repo.Get().Count() == 2);
-
-            var hero = new Hero();
-
-            repo.Insert(hero);
-            Assert.IsTrue(repo.Get().Count() == 3);
-            mock.Verify(m => m.Insert(It.IsAny<Hero>()), Times.Once());
-
-            repo.Delete(hero);
-            Assert.IsTrue(repo.Get().Count() == 2);
-            mock.Verify(m => m.Delete(It.IsAny<Hero>()), Times.Once());
-        }
-    }
 }
--- DAL/Repositories/Repository.cs
+++ DAL/Repositories/Repository.cs
@@ -7,22 +7,6 @@ using System.Linq.Expressions;
 
 namespace DAL.Repositories
 {
-    public interface IRepository<TEntity> where TEntity : class
-    {
-        IEnumerable<TEntity> Get(
-            Expression<Func<TEntity, bool>> filter = null,
-            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
-            string includeProperties = "");
-
-        TEntity GetById(object id);
-        void Insert(TEntity entity);
-        void Update(TEntity entityToUpdate);
-        void Delete(object id);
-        void Delete(TEntity entityToDelete);
-
-        void Save();
-    }
-
     public class Repository<TEntity> : IRepository<TEntity>
         where TEntity : class
     {
